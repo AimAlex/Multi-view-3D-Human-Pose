be7973421a5932ad851e502f7a745acc075c3ac1
AimAlex <alex-li@sjtu.edu.cn>
2018-10-15 11:00:07 +0800

    may be can train

--- train.py
+++ train.py
@@ -3,14 +3,212 @@ import torch.nn.functional as F
 import torch.utils.data as Data
 from torch.autograd import Variable
 import matplotlib.pyplot as plt
+import json
+import numpy as np
+import cv2 as cv
+import math
+import sympy as sp
+import random
+
+deviation = 15.577630734654525
+subdeviation = math.sqrt(deviation * deviation / 2)
+torch.set_default_tensor_type('torch.DoubleTensor')
+
+def GaussianConf(wx, wy):
+	w = math.sqrt(wx * wx + wy * wy)
+	c = 1 - w / (2 * deviation)
+	return max(c, 0)
+
+
+class keyPoint:
+	def __init__(self,xx, yy, confidence):
+		self.x = xx
+		self.y = yy
+		self.conf = confidence
+
+class humanPoint:
+	def __init__(self, Points):
+		self.points = Points
+		self.cam2 = 0
+		self.cam3 = 0
+
+	def init_cam1(self):
+		points2 = [keyPoint(0, 0, 0) for i in range(10)]
+		points3 = [keyPoint(0, 0, 0) for i in range(10)]
+		self.cam2 = humanPoint(points2)
+		self.cam3 = humanPoint(points3)
+
+class photoHuman:
+	def __init__(self):
+		self.human = []
+
+class keyPoint3D:
+	def __init__(self,xx, yy, zz):
+		self.x = xx
+		self.y = yy
+		self.z = zz
+
+class humanPoint3D:
+	def __init__(self, Points):
+		self.points = Points
+
+class photoHuman3D:
+	def __init__(self):
+		self.human = []
+
+
+#ground truth
+gdTruthFile = open("./data/camma_mvor_2018.json", "rb")
+gdTruthJson = json.load(gdTruthFile)
+
+cam2d = gdTruthJson["annotations"]
+
+gt_cam1photo = [photoHuman() for i in range(57)]
+
+#read cam1 ground truth
+for cam in cam2d:
+	if int(cam["image_id"]/10**10) != 1:
+		#print(int(cam["image_id"] / 10**10))
+		continue
+	view = int(cam["image_id"]/10**7) % 10
+	if view != 1:
+		continue
+
+	photo = int(cam["image_id"]) % 1000
+	humanID = cam["person_id"]
+	pointList = [1] * 10
+	keyList = cam["keypoints"]
+
+	for i in range(10):
+		if keyList[3 * i + 2] == 0:
+			pointList[i] = keyPoint(0, 0, 0)
+			continue
+		wx = random.gauss(0, subdeviation)
+		wy = random.gauss(0, subdeviation)
+		pointList[i] = keyPoint(keyList[3 * i] + wx, keyList[3 * i + 1] + wy, GaussianConf(wx, wy)) 
+
+	human = humanPoint(pointList)
+	human.init_cam1()
+
+	if humanID >= len(gt_cam1photo[photo].human):
+		gt_cam1photo[photo].human.append(human)
+	else:
+		gt_cam1photo[photo].human.insert(humanID, human)
+
+#read cam2 & cam3 ground truth and match
+for cam in cam2d:
+	if int(cam["image_id"]/10**10) != 1:
+		#print(int(cam["image_id"] / 10**10))
+		continue
+	view = int(cam["image_id"]/10**7) % 10
+	if view == 1:
+		continue
+
+	photo = int(cam["image_id"]) % 1000
+	humanID = cam["person_id"]
+	if humanID < 0:
+		continue
+	pointList = [1] * 10
+	keyList = cam["keypoints"]
+
+	for i in range(10):
+		if keyList[3 * i + 2] == 0:
+			pointList[i] = keyPoint(0, 0, 0)
+			continue
+		wx = random.gauss(0,subdeviation)
+		wy = random.gauss(0,subdeviation)
+		pointList[i] = keyPoint(keyList[3 * i] + wx, keyList[3 * i + 1] + wy, GaussianConf(wx, wy))
+
+	#print(pointList)
+	human = humanPoint(pointList)
+	#print (gt_cam1photo[1].human[0].cam2, gt_cam1photo[1].human[0].cam3)
+	#print(humanID, photo, len(gt_cam1photo[photo].human), cam["image_id"])
+	if view == 2:
+		if len(gt_cam1photo[photo].human) <= humanID:
+			faker = humanPoint([keyPoint(0, 0, 0) for i in range(10)])
+			faker.init_cam1()
+			gt_cam1photo[photo].human.insert(humanID, faker)
+		gt_cam1photo[photo].human[humanID].cam2 = human
+	else :
+		#print (gt_cam1photo[1].human[0].cam2, gt_cam1photo[1].human[0].cam3, photo, humanID)
+		if len(gt_cam1photo[photo].human) <= humanID:
+			faker = humanPoint([keyPoint(0, 0, 0) for i in range(10)])
+			faker.init_cam1()
+			gt_cam1photo[photo].human.insert(humanID, faker)
+		gt_cam1photo[photo].human[humanID].cam3 = human
+
+
+cam3d = gdTruthJson["annotations3D"]
+
+gt_3dphoto = [photoHuman3D() for i in range(57)]
+
+for cam in cam3d:
+	str = cam["image_ids"]
+	if str[0] != '1':
+		continue
+	#print(int(str[8:11]))
+	photo = int(str[8:11])
+	humanID = cam["person_id"]
+	pointList = [1] * 10
+	keyList = cam["keypoints3D"]
+
+	pointList[0] = keyPoint3D(keyList[0], keyList[1], keyList[2])
+	pointList[1] = keyPoint3D(keyList[4], keyList[5], keyList[6])
+	pointList[2] = keyPoint3D(keyList[8], keyList[9], keyList[10])
+	pointList[3] = keyPoint3D(keyList[12], keyList[13], keyList[14])
+	pointList[4] = keyPoint3D(keyList[16], keyList[17], keyList[18])
+	pointList[5] = keyPoint3D(keyList[20], keyList[21], keyList[22])
+	pointList[6] = keyPoint3D(keyList[24], keyList[25], keyList[26])
+	pointList[7] = keyPoint3D(keyList[28], keyList[29], keyList[30])
+	pointList[8] = keyPoint3D(keyList[32], keyList[33], keyList[34])
+	pointList[9] = keyPoint3D(keyList[36], keyList[37], keyList[38])
+
+	human = humanPoint3D(pointList)
+	if humanID >= len(gt_3dphoto[photo].human):
+		gt_3dphoto[photo].human.append(human)
+	else:
+		gt_3dphoto[photo].human.insert(humanID, human)
+
+train_x = []
+train_y = []
+for i in range(57):
+	for j in range(len(gt_cam1photo[i].human)):
+		if j >= len(gt_3dphoto[i].human):
+			break
+		p = []
+		#print (gt_cam1photo[1].human[0].cam2, gt_cam1photo[1].human[0].cam3)
+		for k in range(10):
+			p.append(gt_cam1photo[i].human[j].points[k].x)
+			p.append(gt_cam1photo[i].human[j].points[k].y)
+			p.append(gt_cam1photo[i].human[j].points[k].conf)
+		human = gt_cam1photo[i].human[j].cam2
+		for k in range(10):
+			#print(human)
+			p.append(human.points[k].x)
+			p.append(human.points[k].y)
+			p.append(human.points[k].conf)
+		human = gt_cam1photo[i].human[j].cam3
+		for k in range(10):
+			p.append(human.points[k].x)
+			p.append(human.points[k].y)
+			p.append(human.points[k].conf)
+		train_x.append(np.array(p))
+
+		q = []
+		for k in range(10):
+			q.append(gt_3dphoto[i].human[j].points[k].x)
+			q.append(gt_3dphoto[i].human[j].points[k].y)
+			q.append(gt_3dphoto[i].human[j].points[k].z)
+		train_y.append(np.array(q))
 
 class MultiLossFunc(torch.nn.Module):
 	def __init__(self):
+		super(MultiLossFunc, self).__init__()
 		return
 
-	def forward(self, x1, x2, x3, x4, y):
+	def forward(self, x1, x2, x3, y):
 		los = torch.nn.MSELoss()
-		return (los(x1, y) + los(x2, y) + los(x3, y) + los(x4, y))
+		return (los(x1, y) + los(x2, y) + los(x3, y))
 
 class MyDataset(Data.Dataset):
     def __init__(self, xx, yy):
@@ -18,7 +216,7 @@ class MyDataset(Data.Dataset):
         self.train_y = yy
 
     def __getitem__(self, index):
-        b_x, b_y = self.train_x[index], self.train_y[index]
+        b_x, b_y = torch.from_numpy(self.train_x[index]).double(), torch.from_numpy(self.train_y[index]).double()
         return b_x, b_y
 
     def __len__(self):
@@ -31,36 +229,75 @@ class Net(torch.nn.Module):
 		self.fc2 = torch.nn.Linear(n_hidden, n_hidden)
 		self.fc3 = torch.nn.Linear(n_hidden, n_hidden)
 		self.fc4 = torch.nn.Linear(n_hidden, n_output)
+
+		self.fc5 = torch.nn.Linear(n_output, n_hidden)
+		self.fc6 = torch.nn.Linear(n_hidden, n_hidden)
+		self.fc7 = torch.nn.Linear(n_hidden, n_hidden)
+		self.fc8 = torch.nn.Linear(n_hidden, n_output)
+
+		self.fc9 = torch.nn.Linear(n_output, n_hidden)
+		self.fc10 = torch.nn.Linear(n_hidden, n_hidden)
+		self.fc11 = torch.nn.Linear(n_hidden, n_hidden)
+		self.fc12 = torch.nn.Linear(n_hidden, n_output)
+
 	def forward(self, x):
 		x1 = F.relu(self.fc1(x))
-		x2 = F.relu(self.fc2(x))
-		x3 = F.relu(self.fc3(x))
-		x4 = F.relu(self.fc4(x))
-		return x1, x2, x_3, x_4
+		x1 = F.relu(self.fc2(x1))
+		x1 = F.relu(self.fc3(x1))
+		x1 = F.relu(self.fc4(x1))
+
+		x2 = F.relu(self.fc5(x1))
+		x2 = F.relu(self.fc6(x2))
+		x2 = F.relu(self.fc7(x2))
+		x2 = F.relu(self.fc8(x2))
+
+		x3 = F.relu(self.fc9(x2))
+		x3 = F.relu(self.fc10(x3))
+		x3 = F.relu(self.fc11(x3))
+		x3 = F.relu(self.fc12(x3))
+
+		return x1, x2, x3
 
 net = Net(90, 1024, 30)
-net.cuda()
+#print(net)
+#net.cuda()
 
 dataset = MyDataset(train_x, train_y)
-train_loader = Data.DataLoader(dataset, batch_size = 10, shuffle = True)
+train_loader = Data.DataLoader(dataset, batch_size = 5, shuffle = True)
 optimizer =  torch.optim.Adam(net.parameters(), lr = 0.001)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=5, gamma=0.5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.7)
 loss_function = MultiLossFunc()
 
 
 
 for t in range(200):
+	print("epoch:", t)
+	k = 0
+	num = 0
 	for step, (x, y) in enumerate(train_loader):
-		b_x = x.cuda
-		b_y = y.cuda
+		b_x = x#.cuda
+		b_y = y#.cuda
 
+		#print(b_x)
 		prediction = net(b_x)
-		loss = loss_function(prediction, b_y)
+		loss = loss_function(prediction[0], prediction[1], prediction[2], b_y.double())
+		
+		k = prediction[2] - b_y
+
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step()
 		scheduler.step()
-
+	lo = 0
+	for j in range(10):
+		test_x = torch.from_numpy(train_x[j+40]).double()
+		test_y = torch.from_numpy(train_y[j+40]).double()
+		output = net(test_x)
+	
+	#print(test_x, test_y)
+		for i in range(30):
+			lo += (test_y[i] - output[2][i]) * (test_y[i] - output[2][i])
+	print(math.sqrt(lo / 100))
 
 
 
